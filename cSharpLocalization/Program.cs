86151cb848fd87e099a437191c0817e398e6e240
silbersteine13 <108279588+silbersteine13@users.noreply.github.com>
2023-02-28 21:03:26 -0400

    Complete degree conversion and check function

--- cSharpLocalization/Program.cs
+++ cSharpLocalization/Program.cs
@@ -6,10 +6,10 @@ class Program
     //Main calls all the other functions
     static void Main()
     {
-        //string firstName = FirstName_Subroutine(); //Variable is imported from subroutine
-        //string lastName = LastName_Subroutine(); //Variable is improted from subroutine
-        //Salutations(firstName, lastName); //Function Salutations uses passed parameters to write a greeting to the user
-        TemperatureCheck();
+        string firstName = FirstName_Subroutine(); //Variable is imported from subroutine
+        string lastName = LastName_Subroutine(); //Variable is improted from subroutine
+        Salutations(firstName, lastName); //Function Salutations uses passed parameters to write a greeting to the user
+        //TemperatureCheck();
 
     }
 
@@ -37,22 +37,38 @@ class Program
         Console.WriteLine(output);
     }
 
-    //This function asks user for the temperature and then converts it to F or C depending on user input
+    //This function asks user for the temperature and then converts it to F or C depending on user input. Then offers the user the option to convert it to Celsius. 
     static void TemperatureCheck()
     {
-        Console.WriteLine("What is the temperature?");
-        int temperatureDegree = Convert.ToInt32(Console.ReadLine()); //Use method Convert.ToInt32 to change input type from String to Integer.
-        Console.WriteLine("The temperature is " + temperatureDegree + "... Is that in Fahrenheit or Celsius? Write F or C then hit ENTER");
-        string temperatureType = Console.ReadLine(); //user writes F or C and inputs variable value
-
-        //if/else statement evaluating if they input F or C
-        if (temperatureType == "F")
-        {
-            Console.WriteLine("Okay. So it is " + temperatureDegree + " degrees Fahrenheit. Big difference from " + temperatureDegree + " degrees Celsius!!!");
+        Console.WriteLine("What is the temperature in Fahrenheit where you're at, right now? Quick! It's urgent!");
+        int temperatureDegree = Convert.ToInt32(Console.ReadLine()); //Variable is the temperature in Fahreinheit that user inputs. Use method Convert.ToInt32 to change input type from String to Integer.
+        
+        //If/else statement with four different responses depending on the temperature the user inputs
+        if (temperatureDegree < 36) {
+            Console.WriteLine("Wow! " + temperatureDegree + " degrees Fahrernheit! That's freezing!!!");
+        } 
+            else if (temperatureDegree > 36 && temperatureDegree < 70) { 
+            Console.WriteLine("Okay," + temperatureDegree + " degrees Fahrernheit isn't exactly warm. Don't forget a coat!");
         }
-        else
-        {
-            Console.WriteLine("Okay. So it is " + temperatureDegree + " degrees Celsius. Big difference from " + temperatureDegree + " degrees Fahrenheit!!!");
+            else if (temperatureDegree > 70 && temperatureDegree < 88) {
+            Console.WriteLine(temperatureDegree + " degrees Fahrernheit. That's quite warm. Don't forget your sunscreen!");
+        } 
+            else {
+            Console.WriteLine("OMG!!!" + temperatureDegree + " degrees Fahrernheit is way too hot! Stay indoors!!!");
+        }
+        Console.WriteLine("Hit ENTER for good news.");
+        Console.ReadLine();
+
+        //logic for converting temperature to celsius
+        Console.WriteLine("Temperatures don't sound as extreme when converted to Celsius. Would you like me to convert the temperature to Celsius? Write Y for YES and N for NO.");
+        string answer = Console.ReadLine();
+        if (answer == "Y") {
+            int temperatureMinus = temperatureDegree - 32; //create this variable first to abide by order of operations in celsius conversion.
+            int temperatureCelsius = temperatureMinus * 5 / 9;
+            Console.WriteLine(temperatureDegree + " degrees Fahreinheit = " + temperatureCelsius + " degrees Celcius");
+        }
+        else { 
+            Console.WriteLine("I didn't want to do it anyay.");
         }
     }
     
